9d1b568d89dc4843f25345473e4ea4f72287de9a
this-is-yogesh <ayogesh1214@gmail.com>
2023-12-19 11:03:47 +0530

    commiting

---
+++ Blog-App/src/conf/conf.js
@@ -0,0 +1,7 @@
+export const configuration = {
+  appwrite_projectid: String(import.meta.env.VITE_PROJECT_ID),
+  appwrite_baseurl: String(import.meta.env.VITE_BASEURL),
+  appwrite_databaseid: String(import.meta.env.VITE_DATABASE_ID),
+  appwrite_collectionid: String(import.meta.env.VITE_COLLECTION_ID),
+  appwrite_bucketid: String(import.meta.env.VITE_BUCKET_ID),
+};
