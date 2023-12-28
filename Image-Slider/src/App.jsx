c0a895a460debcfb4df081713cd84a7084cf06a1
this-is-yogesh <ayogesh1214@gmail.com>
2023-12-28 16:59:29 +0530

    Pushing changes for Image Slider

--- Image-Slider/src/App.jsx
+++ Image-Slider/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { data } from "./data";
+import { useEffect } from "react";
+import { FaArrowLeft, FaArrowRight, FaCircle } from "react-icons/fa";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [da, setData] = useState([]);
+  const [current, setCurrent] = useState(0);
 
+  useEffect(() => {
+    setData(data);
+    setCurrent(1);
+  }, []);
+
+  function handleslideleft(id) {
+    setCurrent(id === 0 ? data.length - 1 : id - 1);
+  }
+
+  function handleslideright(id) {
+    setCurrent(id === data.length - 1 ? 0 : id + 1);
+  }
+
+  function handleclick(id) {
+    setCurrent(id);
+  }
+  console.log(current, "current");
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main">
+      <FaArrowLeft className="left" onClick={() => handleslideleft(current)} />
+      <div className="imagebox">
+        {da.map((d, index) => (
+          <>
+            <div className="image">
+              <img
+                src={d.url}
+                alt="img"
+                key={d.id}
+                hidden={index !== current}
+              />
+            </div>
+          </>
+        ))}
+        <div className="circle">
+          {da.map((_, index) => (
+            <FaCircle
+              className={index === current ? "active" : ""}
+              onClick={() => handleclick(index)}
+            />
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <FaArrowRight
+        className="right"
+        onClick={() => handleslideright(current)}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Image-Slider/src/data.js
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    id: "0",
+    url: "https://images.unsplash.com/photo-1699942681763-d1da9f692489?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "1",
+    url: "https://images.unsplash.com/photo-1703607518981-b3c1fdb1f740?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8",
+  },
+  {
+    id: "2",
+    url: "https://images.unsplash.com/photo-1697665666330-7acf230fa830?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "3",
+    url: "https://images.unsplash.com/photo-1683009427666-340595e57e43?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8",
+  },
+  {
+    id: "4",
+    url: "https://plus.unsplash.com/premium_photo-1679064286720-9f28c0f012d8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8",
+  },
+];
