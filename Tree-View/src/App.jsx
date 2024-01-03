24b94b8faad6ac5551e7242d0fd919c104a60656
this-is-yogesh <ayogesh1214@gmail.com>
2024-01-03 12:01:34 +0530

    recursive call

--- Tree-View/src/App.jsx
+++ Tree-View/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import backgroundImage from "./assets/download.jpeg"
+import menus from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(menus);
 
+  const showdata = (index) => {
+    if (data[index].children && data[index].children.length) {
+      setData(data[index].children);
+    }
+  };
+
+  console.log(data, "Data");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+
+      <div style={{backgroundImage:`url(${bannerImage})`}}>
+
+
+      {data &&
+        data.map((i, index) => {
+          return (
+            <div key={index} className="container">
+              <p> {i.label}</p>
+              <button onClick={() => showdata(index)}> + </button>
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
