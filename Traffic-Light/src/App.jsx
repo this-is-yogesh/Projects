ccb43ea93d5acc915741f82968982f0001738d6a
this-is-yogesh <ayogesh1214@gmail.com>
2024-01-19 18:34:15 +0530

    Built Traffic Light Stimulator

--- Traffic-Light/src/App.jsx
+++ Traffic-Light/src/App.jsx
@@ -1,16 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentcolor, setCurrentColor] = useState("green");
+  const light = {
+    red: {
+      backgroundColor: "red",
+      duration: 4000,
+      next: "yellow",
+    },
+    yellow: {
+      backgroundColor: "yellow",
+      duration: 3000,
+      next: "green",
+    },
+    green: {
+      backgroundColor: "green",
+      duration: 2000,
+      next: "red",
+    },
+  };
 
-return(
-  <div>
-    
-  </div>
-)
+  useEffect(() => {
+    const { duration, next } = light[currentcolor];
+    const timer = setTimeout(() => {
+      setCurrentColor(next);
+    }, duration);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [currentcolor]);
+
+  return (
+    <div className="wrapper">
+      {Object.keys(light).map((m) => (
+        <div
+          className="traffic-light"
+          style={{
+            backgroundColor:
+              light[m].backgroundColor === currentcolor &&
+              light[m].backgroundColor,
+          }}
+        ></div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
